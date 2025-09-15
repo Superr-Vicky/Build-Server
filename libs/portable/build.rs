cbd30843a8f8f3eafd49bd838eb033074ed1f1ab
Turing <turing@xusw.com>
2025-09-15 18:05:30 +0800

    feat(portable): add dynamic Windows resource metadata configuration - Comment out static metadata in Cargo.toml - Add dynamic resource setting in build.rs using environment variables - Support CUSTOM_APP_NAME and CUSTOM_APP_DESCRIPTION for customization - Fallback to default RustDesk values when env vars not set

--- libs/portable/build.rs
+++ libs/portable/build.rs
@@ -9,6 +9,12 @@ fn main() {
                 winapi::um::winnt::SUBLANG_ENGLISH_US,
             ))
             .set_manifest_file("../../res/manifest.xml");
+        let product_name = option_env!("CUSTOM_APP_NAME").unwrap_or("RustDesk");
+        let product_description = option_env!("CUSTOM_APP_DESCRIPTION").unwrap_or("RustDesk Remote Desktop");
+        res.set("LegalCopyright", &format!("Copyright © 2025 {}. All rights reserved.", product_name));
+        res.set("ProductName", product_name);
+        res.set("OriginalFilename", &format!("{}.exe", product_name));
+        res.set("FileDescription", product_description);
         match res.compile() {
             Err(e) => {
                 write!(std::io::stderr(), "{}", e).unwrap();
