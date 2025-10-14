22e7da7c9c118abf9f005b99cf17412f7c8f5a73
Turing <turing@xusw.com>
2025-10-15 00:47:10 +0800

    chore(build): update header and handle empty product name
    - Update build.rs header information.
    - Improve handling of CUSTOM_APP_NAME and CUSTOM_APP_DESCRIPTION,
      ensuring default values are used when environment variables are empty
      or undefined. Fixes issues introduced in cbd30843a8f8f3eafd49bd838eb033074ed1f1ab.

--- libs/portable/build.rs
+++ libs/portable/build.rs
@@ -9,8 +9,8 @@ fn main() {
                 winapi::um::winnt::SUBLANG_ENGLISH_US,
             ))
             .set_manifest_file("../../res/manifest.xml");
-        let product_name = option_env!("CUSTOM_APP_NAME").unwrap_or("RustDesk");
-        let product_description = option_env!("CUSTOM_APP_DESCRIPTION").unwrap_or("RustDesk Remote Desktop");
+        let product_name = option_env!("CUSTOM_APP_NAME").filter(|s| !s.is_empty()).unwrap_or("RustDesk");
+        let product_description = option_env!("CUSTOM_APP_DESCRIPTION").filter(|s| !s.is_empty()).unwrap_or("RustDesk Remote Desktop");
         res.set("LegalCopyright", &format!("Copyright © 2025 {}. All rights reserved.", product_name));
         res.set("ProductName", product_name);
         res.set("OriginalFilename", &format!("{}.exe", product_name));

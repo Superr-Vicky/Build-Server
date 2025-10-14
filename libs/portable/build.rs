Superr-Vicky/Build-Server

commit cbd30843a8f8f3eafd49bd838eb033074ed1f1ab

feat(portable): add dynamic Windows resource metadata configuration - Comment out static metadata in Cargo.toml - Add dynamic resource setting in build.rs using environment variables - Support CUSTOM_APP_NAME and CUSTOM_APP_DESCRIPTION for customization - Fallback to default RustDesk values when env vars not set

libs/portable/build.rs
@@ -9,6 +9,12 @@ fn main() {
                 winapi::um::winnt::SUBLANG_ENGLISH_US,
             ))
             .set_manifest_file("../../res/manifest.xml");
+        let product_name = option_env!("CUSTOM_APP_NAME").unwrap_or("RustDesk");
+        let product_description = option_env!("CUSTOM_APP_DESCRIPTION").unwrap_or("RustDesk Remote Desktop");
+        res.set("LegalCopyright", &format!("Copyright © 2025 {}. All rights reserved.", product_name));
+        res.set("ProductName", product_name);
+        res.set("OriginalFilename", &format!("{}.exe", product_name));
+        res.set("FileDescription", product_description);
         match res.compile() {
             Err(e) => {
                 write!(std::io::stderr(), "{}", e).unwrap();

libs/portable/Cargo.toml
@@ -18,10 +18,10 @@ winapi = { version = "0.3", features = ["winbase"] }
 native-windows-gui = {version = "1.0", default-features = false, features = ["animation-timer", "image-decoder"]}
 
 [package.metadata.winres]
-LegalCopyright = "Copyright © 2025 Purslane Ltd. All rights reserved."
-ProductName = "RustDesk"
-OriginalFilename = "rustdesk.exe"
-FileDescription = "RustDesk Remote Desktop"
+#LegalCopyright = "Copyright © 2025 Purslane Ltd. All rights reserved."
+#ProductName = "RustDesk"
+#OriginalFilename = "rustdesk.exe"
+#FileDescription = "RustDesk Remote Desktop"
 #ProductVersion = ""
 
 [target.'cfg(target_os="windows")'.build-dependencies]

commit 22e7da7c9c118abf9f005b99cf17412f7c8f5a73

chore(build): update header and handle empty product name
- Update build.rs header information.
- Improve handling of CUSTOM_APP_NAME and CUSTOM_APP_DESCRIPTION,
  ensuring default values are used when environment variables are empty
  or undefined. Fixes issues introduced in cbd30843a8f8f3eafd49bd838eb033074ed1f1ab.

libs/portable/build.rs
@@ -9,8 +9,8 @@ fn main() {
                 winapi::um::winnt::SUBLANG_ENGLISH_US,
             ))
             .set_manifest_file("../../res/manifest.xml");
-        let product_name = option_env!("CUSTOM_APP_NAME").unwrap_or("RustDesk");
-        let product_description = option_env!("CUSTOM_APP_DESCRIPTION").unwrap_or("RustDesk Remote Desktop");
+        let product_name = option_env!("CUSTOM_APP_NAME").filter(|s| !s.is_empty()).unwrap_or("RustDesk");
+        let product_description = option_env!("CUSTOM_APP_DESCRIPTION").filter(|s| !s.is_empty()).unwrap_or("RustDesk Remote Desktop");
         res.set("LegalCopyright", &format!("Copyright © 2025 {}. All rights reserved.", product_name));
         res.set("ProductName", product_name);
         res.set("OriginalFilename", &format!("{}.exe", product_name));